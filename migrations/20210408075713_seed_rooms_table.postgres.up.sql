f6ba97de30892816b52150fd01c310fd649f0ba9
Rasyad <rasyadsubandrio@gmail.com>
2021-04-08 19:32:56 +0800

    115 Seperate .js files from html templates

---
+++ migrations/20210408075713_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Major''s Suite','2021-01-01 00:00:00.000','2021-01-01 00:00:00.000'),
+	 ('General''s Quarters','2021-01-01 00:00:00.000','2021-01-01 00:00:00.000');
---
+++ migrations/20210408075840_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Owner Block','2021-04-07 00:00:00.000','2021-04-07 00:00:00.000'),
+	 ('Reservation','2021-01-01 00:00:00.000','2021-01-01 00:00:00.000');
